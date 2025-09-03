fd8aa66ba5b58e841d2d866c6ba047ce3551a39b
Dishan Bhattacharya <dishanbhattacharya05@gmail.com>
2025-09-03 11:38:58 -0400

    Fixed linkedin link

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.linkedin.com/in/dishanb/"
+          href="https://www.linkedin.com/in/disbh/"
           target="_blank"
           rel="noopener noreferrer"
         >
